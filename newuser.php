9aa13881aa96fbe447c05091ac91c18bc0ee1651
Ravi Kumar <ravikumar_csd@srkrec.edu.in>
2022-12-28 22:05:47 +0530

    finql

--- newuser.php
+++ newuser.php
@@ -1,21 +1,16 @@
 <?php
-echo "Jai Sri Ram";
-$con=new mysqli("43.250.40.60","bo_housie","bo_housie@421","srkr_housie");
-if($con){
-    echo "Ohm Namo Shivaya";
-    if($_POST['submit']){
-        echo "jai Sri ram Jai Jai Sri Ram";
-        $pid = $_POST['pid'];
-        $name = $_POST['name'];
-        $place = $_POST['place'];
-        $pin = $_POST['pin'];
-        $status = $_POST['status'];
-        $quary = "insert into players ('pid','player_name','place','pin','status') values ('$pid','$name','$place','$pin',$status)";
-        $run = mysqli_query($con, $quary);
-        if($run){
-            echo "Jai Jai Bhavani";
-        }
+$con = new mysqli("localhost", "root", "", "srkr_housie");
+if ($_POST['submit']) {
+    $pid = $_POST['pid'];
+    $name = $_POST['name'];
+    $place = $_POST['place'];
+    $pin = $_POST['pin'];
+    $status = $_POST['status'];
+    $quary = "insert into players (`pid`,`player_name`,`place`,`pin`,`status`) VALUES ('$pid','$name','$place','$pin',$status);";
+    $run = mysqli_query($con,$quary);
+    if($run){
+        echo "<script>alert('Data Entered Sucessfully')</script>";
+        // header("Location:newuser.html");
     }
 }
 ?>
- 
