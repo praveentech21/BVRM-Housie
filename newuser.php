7f91e0f2911491f8f6bfa9a2ca50078e603c65cb
Praveentech21 <ravikuar_csd@srkrec.edu.in>
2023-05-12 11:51:31 +0530

    Jia Jia Shankara

--- newuser.php
+++ newuser.php
@@ -4,13 +4,11 @@ if ($_POST['submit']) {
     $pid = $_POST['pid'];
     $name = $_POST['name'];
     $place = $_POST['place'];
-    $pin = $_POST['pin'];
-    $status = $_POST['status'];
-    $quary = "insert into players (`pid`,`player_name`,`place`,`pin`,`status`) VALUES ('$pid','$name','$place','$pin',$status);";
+    $quary = "insert into players (`pid`,`player_name`,`place`,`pin`,`status`) VALUES ('$pid','$name','$place',2023,1);";
     $run = mysqli_query($con,$quary);
     if($run){
         echo "<script>alert('Data Entered Sucessfully')</script>";
-        // header("Location:newuser.html");
     }
+    
 }
 ?>
